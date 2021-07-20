182657373cbcb8b67f6bdb64abf12e8a201846df
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-07-20 11:03:11 +0200

    Dynamic menu

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -4,41 +4,18 @@
       <img src="~assets/logo.png" alt="Logo">
     </NuxtLink>
 
-    <div v-if="menu" class="menu" @click="toggle">
-      <NuxtLink to="/">
-        Welkom
-      </NuxtLink>
-      <NuxtLink to="/oude-raadhuisplein">
-        Oude Raadhuisplein
-      </NuxtLink>
-      <NuxtLink to="/plein-2000">
-        Plein 2000
-      </NuxtLink>
-      <NuxtLink to="/kindervrijmarkt">
-        Kindervrijmarkt
-      </NuxtLink>
-      <NuxtLink to="/oranjemarkt">
-        Oranjemarkt
-      </NuxtLink>
-      <NuxtLink to="/kermis">
-        Kermis
-      </NuxtLink>
-      <NuxtLink to="/taptoe">
-        Taptoe
-      </NuxtLink>
-      <NuxtLink to="/fotos">
-        Foto's
-      </NuxtLink>
-      <NuxtLink to="/toiletten">
-        Toiletten
-      </NuxtLink>
-      <NuxtLink to="/verkeer-en-parkeren">
-        Verkeer en Parkeren
-      </NuxtLink>
-      <NuxtLink to="/contact">
-        Contact
-      </NuxtLink>
+    <div class="menu">
+      <NuxtLink v-for="item in menuItems" :key="item.id" :to="item.url">
+        {{ item.title }}
+      </Nuxtlink>
     </div>
+
+    <div v-if="menu" class="resmenu" @click="toggle">
+      <NuxtLink v-for="item in menuItems" :key="item.id" :to="item.url">
+        {{ item.title }}
+      </Nuxtlink>
+    </div>
+
     <div class="menuButton" @click="toggle">
       Menu
     </div>
@@ -49,6 +26,26 @@
 import { mapMutations } from 'vuex'
 
 export default {
+  // Menu Data
+  data () {
+    return {
+      menuItems: [
+        { id: 0, title: 'Welkom', url: '/' },
+        { id: 1, title: 'Oude Raadhuisplein', url: '/oude-raadhuisplein' },
+        { id: 2, title: 'Plein 2000', url: '/plein-2000' },
+        { id: 3, title: 'Kindervrijmarkt', url: '/kindervrijmarkt' },
+        { id: 4, title: 'Oranjemarkt', url: '/oranjemarkt' },
+        { id: 5, title: 'Kermis', url: '/kermis' },
+        { id: 6, title: 'Taptoe', url: '/Taptoe' },
+        { id: 7, title: 'Foto\'s', url: '/fotos' },
+        { id: 8, title: 'Toiletten', url: '/toiletten' },
+        { id: 9, title: 'Verkeer en Parkeren', url: '/verkeer-en-parkeren' },
+        { id: 10, title: 'Contact', url: '/contact' }
+      ]
+    }
+  },
+
+  // Menu logic
   computed: {
     menu () {
       return this.$store.state.menu.isOpen
@@ -96,12 +93,14 @@ export default {
 .nav .menu a {color: #fff; font-size: 14px; display: block; text-decoration: none; outline: none; font-family: "Open Sans",Helvetica,Arial,Sans-Serif; display: block; padding: 10px; line-height: 1; border-radius: 3px; transition: all 0.3s ease 0s; font-weight: 700; margin-left: 10px;}
 .nav .menu a.nuxt-link-exact-active {background: rgba(255,255,255,.15);}
 
+.nav .resmenu,
 .nav .menuButton {display: none;}
 
 @media (max-width: 760px) {
+  .nav .menu {display: none;}
   .nav .menuButton {display: block; margin: auto 0 auto auto; background: rgba(255,255,255,.15); color: #fff; padding: 10px; line-height: 1; font-size: 14px; font-weight: bold; border-radius: 3px;}
-  .nav .menu {flex-direction: column; height: calc(100vh - 100px); overflow-y: auto; padding-top: 30px; box-sizing: border-box; position: absolute; top: 100px; left: 0; right: 0; background: #fff;}
-  .nav .menu a {color: #000; display: block; border-bottom: 1px solid #eee; padding: 10px 0;}
-  .nav .menu a.nuxt-link-exact-active {color: #fc8100;}
+  .nav .resmenu {display: block; height: calc(100vh - 100px); overflow-y: auto; padding-top: 30px; box-sizing: border-box; position: absolute; top: 100px; left: 0; right: 0; background: #fff;}
+  .nav .resmenu a {color: #000; font-weight: bold; display: block; border-bottom: 1px solid #eee; padding: 10px; text-decoration: none;}
+  .nav .resmenu a.nuxt-link-exact-active {color: #fc8100;}
 }
 </style>
