6004506471325814fce81e0481e1a13d9218a908
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-07-20 22:41:30 +0200

    Added 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <h1 v-if="error.statusCode === 404">
+      Helaas!
+    </h1>
+    <h1 v-else>
+      Er is een fout opgetreden
+    </h1>
+    <p>Deze pagina bestaat niet.</p>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  layout: 'error',
+  props: {
+    error: { type: Object, required: true }
+  }
+}
+
+</script>
