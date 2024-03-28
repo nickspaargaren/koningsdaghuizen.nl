d5199d1306c83508bb73bbf0386eedee93090bd7
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-03-28 21:08:47 +0100

    Remove old html elements

--- pages/contact.vue
+++ pages/contact.vue
@@ -34,69 +34,65 @@ async function onSubmit(event) {
     <h2>Oranje Stichting Huizen</h2>
     <div className="grid md:gap-5 md:grid-cols-2">
       <div>
-        <td class="FCK2kolTd">
-          <p>
-            <strong>Bestuur</strong><br />
-            Voorzitter: Hans Borremans (0651 98 92 18)<br />
-            Penningmeester: Roeland Bijlard (0623 29 85 77)<br />
-            Kandidaat: Leo Detering (0646 42 58 50)
-          </p>
-          <p>
-            <strong>Postadres</strong><br />
-            Postbus 1044<br />
-            1270 BA Huizen
-          </p>
-          <p>
-            <strong>Financiën</strong><br />
-            Roeland Bijlard, penningmeester<br />
-            Tel.:&nbsp; 0623 29 85 77<br />
-            IBAN Oranje stichting Huizen NL88 RABO 0329 9080 81
-          </p>
-          <p>
-            <strong>Mail</strong><br />
-            oranjestichtinghuizen@xs4all.nl<br />
-            info@koningsdaghuizen.nl
-          </p>
-        </td>
+        <p>
+          <strong>Bestuur</strong><br />
+          Voorzitter: Hans Borremans (0651 98 92 18)<br />
+          Penningmeester: Roeland Bijlard (0623 29 85 77)<br />
+          Kandidaat: Leo Detering (0646 42 58 50)
+        </p>
+        <p>
+          <strong>Postadres</strong><br />
+          Postbus 1044<br />
+          1270 BA Huizen
+        </p>
+        <p>
+          <strong>Financiën</strong><br />
+          Roeland Bijlard, penningmeester<br />
+          Tel.:&nbsp; 0623 29 85 77<br />
+          IBAN Oranje stichting Huizen NL88 RABO 0329 9080 81
+        </p>
+        <p>
+          <strong>Mail</strong><br />
+          oranjestichtinghuizen@xs4all.nl<br />
+          info@koningsdaghuizen.nl
+        </p>
       </div>
       <div>
-        <td class="FCK2kolTd">
-          <p>
-            <strong>Contact Koningsmarkt</strong><br />
-            Ga naar
-            <a
-              href="https://www.dynamicpromotions.nl"
-              rel="nofollow"
-              target="_blank"
-              >www.dynamicpromotions.nl</a
-            >&nbsp;voor huur van kramen
-          </p>
-          <p>
-            <strong>Contactpersoon Taptoe</strong><br />
-            Rob Bource<br />
-            Mail: robbource@gmail.com<br />
-            Tel.: 0653 25 30 93
-          </p>
-          <p>
-            <strong>Contactpersoon artiesten</strong><br />
-            Peter Buchner P-PRODUCTIES<br />
-            Mail: peterlot50@gmail.com<br />
-            Tel.: 0653 74 42 06
-          </p>
-          <p>
-            <strong>Contactpersonen middagprogramma Plein 2000</strong><br />
-            Kim Tawjoeram<br />
-            Mail: kim@dynamicarts.nl<br />
-            Lara Campbell<br />
-            Mail: info@laresz.nl
-          </p>
-          <p>
-            <strong>Contactpersoon algemeen Plein 2000</strong><br />
-            Ringo Prins<br />
-            Mail: ringo@grandcafe-heertje.nl<br />
-            Tel.: 0642 62 46 01
-          </p>
-        </td>
+        <p>
+          <strong>Contact Koningsmarkt</strong><br />
+          Ga naar
+          <a
+            href="https://www.dynamicpromotions.nl"
+            rel="nofollow"
+            target="_blank"
+            >www.dynamicpromotions.nl</a
+          >&nbsp;voor huur van kramen
+        </p>
+        <p>
+          <strong>Contactpersoon Taptoe</strong><br />
+          Rob Bource<br />
+          Mail: robbource@gmail.com<br />
+          Tel.: 0653 25 30 93
+        </p>
+        <p>
+          <strong>Contactpersoon artiesten</strong><br />
+          Peter Buchner P-PRODUCTIES<br />
+          Mail: peterlot50@gmail.com<br />
+          Tel.: 0653 74 42 06
+        </p>
+        <p>
+          <strong>Contactpersonen middagprogramma Plein 2000</strong><br />
+          Kim Tawjoeram<br />
+          Mail: kim@dynamicarts.nl<br />
+          Lara Campbell<br />
+          Mail: info@laresz.nl
+        </p>
+        <p>
+          <strong>Contactpersoon algemeen Plein 2000</strong><br />
+          Ringo Prins<br />
+          Mail: ringo@grandcafe-heertje.nl<br />
+          Tel.: 0642 62 46 01
+        </p>
       </div>
       <div>
         <h2>PR</h2>
