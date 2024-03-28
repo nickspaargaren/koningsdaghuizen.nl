0f8a6ef7746ab938b966a63e0770d56cb368250c
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-03-28 20:33:26 +0100

    Add oude raadhuisplein program

--- pages/oude-raadhuisplein.vue
+++ pages/oude-raadhuisplein.vue
@@ -2,8 +2,23 @@
 useHead({
   titleTemplate: () => "Oude raadhuisplein | Koningsdag Huizen",
 });
+
+const { events } = await queryContent("/program/ouderaadhuisplein").findOne();
 </script>
 
 <template>
-  <UContainer>Oude raadhuisplein</UContainer>
+  <UContainer>
+    <h1>Het Oude Raadhuisplein</h1>
+    <h5>
+      10:00 - 18:00 uur<br />
+      Presentatie Eric Holzhauer
+    </h5>
+
+    <div>
+      <div v-for="(event, index) in events" :key="index" class="event">
+        <h3>{{ event.description }}</h3>
+        <p class="text-lg">{{ event.time }}</p>
+      </div>
+    </div>
+  </UContainer>
 </template>
