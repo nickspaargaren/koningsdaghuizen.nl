66d735a96b9b0ea0b54acc791f9c2788fd3df12b
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2024-03-28 21:14:49 +0100

    Update Contact form to own component

---
+++ components/ContactForm.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { z } from "zod";
+import { ref, reactive } from "vue";
+
+const schema = z.object({
+  name: z.string({ required_error: "Dit veld is verplicht" }),
+  phoneNumber: z
+    .string({ required_error: "Dit veld is verplicht" })
+    .min(8, "Moet minimaal 8 tekens lang zijn"),
+  email: z.string({ required_error: "Dit veld is verplicht" }).email(),
+  question: z.string({ required_error: "Dit veld is verplicht" }),
+});
+
+const initialState = {
+  name: undefined,
+  phoneNumber: undefined,
+  email: undefined,
+  question: undefined,
+};
+
+const state = reactive({ ...initialState });
+
+const isOpen = ref(false);
+
+async function onSubmit(event) {
+  isOpen.value = true;
+  Object.assign(state, initialState);
+}
+</script>
+
+<template>
+  <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+    <UFormGroup label="Uw volledige naam" name="name">
+      <UInput v-model="state.name" />
+    </UFormGroup>
+
+    <UFormGroup label="Telefoon nummer" name="phoneNumber">
+      <UInput v-model="state.phoneNumber" />
+    </UFormGroup>
+
+    <UFormGroup label="E-mail adres" name="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Uw vraag of opmerking" name="question">
+      <UInput v-model="state.question" />
+    </UFormGroup>
+
+    <UButton type="submit">Versturen</UButton>
+
+    <UAlert
+      title="Uw bericht is verzonden."
+      description="Bedankt voor uw bericht. We nemen zo snel mogelijk contact met u op."
+      v-if="isOpen"
+    />
+  </UForm>
+</template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,33 +1,3 @@
-<script setup>
-import { z } from "zod";
-import { ref, reactive } from "vue";
-
-const schema = z.object({
-  name: z.string({ required_error: "Dit veld is verplicht" }),
-  phoneNumber: z
-    .string({ required_error: "Dit veld is verplicht" })
-    .min(8, "Moet minimaal 8 tekens lang zijn"),
-  email: z.string({ required_error: "Dit veld is verplicht" }).email(),
-  question: z.string({ required_error: "Dit veld is verplicht" }),
-});
-
-const initialState = {
-  name: undefined,
-  phoneNumber: undefined,
-  email: undefined,
-  question: undefined,
-};
-
-const state = reactive({ ...initialState });
-
-const isOpen = ref(false);
-
-async function onSubmit(event) {
-  isOpen.value = true;
-  Object.assign(state, initialState);
-}
-</script>
-
 <template>
   <UContainer>
     <h1>Contact</h1>
@@ -112,31 +82,6 @@ async function onSubmit(event) {
       Heeft u een vraag over ons product of dienst? Dan kunt u contact met ons
       opnemen via het onderstaande formulier.
     </p>
-    <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
-      <UFormGroup label="Uw volledige naam" name="name">
-        <UInput v-model="state.name" />
-      </UFormGroup>
-
-      <UFormGroup label="Telefoon nummer" name="phoneNumber">
-        <UInput v-model="state.phoneNumber" />
-      </UFormGroup>
-
-      <UFormGroup label="E-mail adres" name="email">
-        <UInput v-model="state.email" />
-      </UFormGroup>
-
-      <UFormGroup label="Uw vraag of opmerking" name="question">
-        <UInput v-model="state.question" />
-      </UFormGroup>
-
-      <UButton type="submit">Versturen</UButton>
-    </UForm>
-  </UContainer>
-  <UContainer>
-    <UAlert
-      title="Uw bericht is verzonden."
-      description="Bedankt voor uw bericht. We nemen zo snel mogelijk contact met u op."
-      v-if="isOpen"
-    />
+    <ContactForm />
   </UContainer>
 </template>
