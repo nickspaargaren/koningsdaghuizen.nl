f61ca5be04966fa2d42972803dd68628482ce69a
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2024-10-05 10:43:54 +0200

    Add playwright homepage screenshot test (#55)

---
+++ playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  snapshotPathTemplate:
+    "{testDir}/__screenshots__/{projectName}/{testName}{ext}",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:3000",
+    // baseURL: "http://host.docker.internal:3000", // macOS
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+  ],
+});
