bc395bfb0da4e3b36a0277695b7341b770b61c14
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2025-04-01 16:39:15 +0200

    Remove unused deprecated key hid in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,6 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
-          hid: 'description',
           name: 'description',
           content: 'Koningsdag Huizen',
         },
